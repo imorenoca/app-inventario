00de80d56b5d7d1183c7b52e6e48efa9c97121c4
imorenoca <imorenoca@outlook.com>
2024-02-17 10:45:55 +0100

    Presentation of Inventory Product List

--- src/app/producto-lista/producto-lista.component.ts
+++ src/app/producto-lista/producto-lista.component.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { Producto } from '../producto';
+import { ProductoService } from '../producto.service';
 
 @Component({
   selector: 'app-producto-lista',
   templateUrl: './producto-lista.component.html',
 })
 export class ProductoListaComponent {
+  productos: Producto[];
+  constructor(private productoServicio: ProductoService){
+  }
+  ngOnInit(){
+    //cargar los productors
+    this.obtenerProductos();
+  }
+
+  private obtenerProductos(){
+    // consumir los datos del observable (suscribirnos)
+    this.productoServicio.obtenerProductosLista().subscribe(
+      (datos =>{
+        this.productos=datos;
+      })
+    )
+
+  }
 
 }
--- src/app/producto.service.ts
+++ src/app/producto.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Producto } from './producto';
 
@@ -12,7 +12,7 @@ export class ProductoService {
   constructor(private clienteHttp: HttpClient) {
 
    }
-   obtenerProductosListas(): Observable<Producto []>{
+   obtenerProductosLista(): Observable<Producto []>{
     return this.clienteHttp.get<Producto[]>(this.urlBase);
   }
 }
