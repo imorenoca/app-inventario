87ac76977e09b327a1a872f54cc7f00ff3ce1b9d
imorenoca <imorenoca@outlook.com>
2024-02-20 12:54:15 +0100

    Add product implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductoListaComponent } from './producto-lista/producto-lista.component';
+import { AgregarProductoComponent } from './agregar-producto/agregar-producto.component';
+import { EditarProductoComponent } from './editar-producto/editar-producto.component';
 
 // configuración de rutas
 const routes: Routes = [
   {path:'productos', component: ProductoListaComponent},
-  {path:'', redirectTo:'productos', pathMatch: 'full'}
+  {path:'', redirectTo:'productos', pathMatch: 'full'},
+  {path:"agregar-producto", component:AgregarProductoComponent},
+  {path: "editar-producto/:id", component:EditarProductoComponent}
 ];
 
 @NgModule({
--- src/app/producto-lista/producto-lista.component.ts
+++ src/app/producto-lista/producto-lista.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Producto } from '../producto';
 import { ProductoService } from '../producto.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-producto-lista',
@@ -8,7 +9,8 @@ import { ProductoService } from '../producto.service';
 })
 export class ProductoListaComponent {
   productos: Producto[];
-  constructor(private productoServicio: ProductoService){
+  constructor(private productoServicio: ProductoService,
+    private enrutador:Router){
   }
   ngOnInit(){
     //cargar los productos
@@ -22,7 +24,9 @@ export class ProductoListaComponent {
         this.productos=datos;
       })
     )
-
+  }
+  editarProducto(id: number){
+    this.enrutador.navigate(['editar-producto', id])
   }
 
 }
